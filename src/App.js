038628c9322fa558bf2822f50cac159d16a9ce0b
YaMamaLul <terentiy51@gmail.com>
2023-05-03 18:11:45 +0500

    firstPages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import './App.css';
+import Home from "./components/screens/home/Home";
+import Registration from "./components/screens/registration/Registration";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/registration" element={<Registration/>}></Route>
+        <Route path='*' element={<div>umer..</div>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,9 @@
+import {NavLink} from "react-router-dom";
+
+const Home = () =>{
+    return <div>
+        <NavLink to='/registration'>toreg</NavLink>
+    </div>
+}
+
+export default Home
