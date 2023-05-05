8a8ca52e1d24f6711b7ae720928b33818aa46987
YaMamaLul <terentiy51@gmail.com>
2023-05-05 18:47:58 +0500

    regPage

--- src/components/screens/registration/Registration.jsx
+++ src/components/screens/registration/Registration.jsx
@@ -1,5 +1,13 @@
+import styles from "./Registration.module.scss"
 const Registration = () =>{
-    return <div>reg</div>
+    return <div className={styles.wrapper}>
+        <img width="70" src="/test-img.svg"/>
+        <div className={styles.header}>Введи логин епту</div>
+        <input className="loginInput"/>
+        <div className={styles.header}>Введи пароль епту</div>
+        <input className="passwordInput"/>
+        <button className={styles.regButton}>Зарегестрируй меня папочка</button>
+    </div>
 }
 
 export default Registration
